b3c2f1128b7dc61ae4e2bcc53f28c1f761fc1c7e
Guilouf <Guilouf@users.noreply.github.com>
2017-10-08 16:20:57 +0200

    - Basic views / editing of tasks

--- hostproject/agendjang/urls.py
+++ hostproject/agendjang/urls.py
@@ -3,5 +3,8 @@ from agendjang import views
 
 
 urlpatterns = [
-    url(r'^tasks$', views.TaskList.as_view(), name='task_list'),
+    # Tasks
+    url(r'^create_task$', views.TaskCreate.as_view(), name='create_task'),
+    url(r'^tasks$', views.TaskList.as_view(), name='list_task'),
+    url(r'^tasks/(?P<pk>\d+)$', views.TaskDetail.as_view(), name='detail_task'),
 ]
--- hostproject/agendjang/views.py
+++ hostproject/agendjang/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
+
 from agendjang.models import Task
+from agendjang.forms import TaskForm
+
+
+class TaskCreate(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy('list_task')
 
 
 class TaskList(ListView):
+    model = Task
+
+
+class TaskDetail(DetailView):
     model = Task
