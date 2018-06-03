ba4ef1cd1b07a2a349179cbd6f045d1201014334
Guilouf <Guilouf@users.noreply.github.com>
2018-06-03 20:15:31 +0200

    - Created ScheduledTaskForm - Clean method that instantiate other task and move their respective dateranges with a timedelta - Removed the exclude

--- hostproject/agendjang/forms.py
+++ hostproject/agendjang/forms.py
@@ -1,12 +1,39 @@
 from django import forms
-from agendjang.models import Task, Tag
+from agendjang.models import Task, ScheduledTask, Tag
+
+from dateutil.relativedelta import relativedelta
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
-        exclude = ['done_date']
+
+
+class ScheduledTaskForm(forms.ModelForm):
+    class Meta:
+        model = ScheduledTask
+        fields = '__all__'
+
+    def clean(self):
+        """
+        Instantiate the tasks based on the scheduled task
+        """
+        if self.cleaned_data['repeats'] != 0:  # finite number of repetitions
+            for i in range(1, self.cleaned_data['repeats']+1):
+                task = Task.objects.create(name=f"{self.cleaned_data['name']}_{i+1}",  # first 1 = linked with schedtask
+                                           description=self.cleaned_data['description'],
+                                           points=self.cleaned_data['points'])
+                task.many_tags.set(list(self.cleaned_data['many_tags']))
+
+                if self.cleaned_data['many_dateranges'].exists():  # checks if the inherited task has a daterange
+                    chron = {self.cleaned_data['chronicity']: i}  # kwargs for relativedelta
+                    # then copy and add a timedelta to these dateranges for the new task (cf Dateranges __add__)
+                    task.many_dateranges.set([(daterange + relativedelta(**chron)).save()  # no full_clean, should be ok
+                                              for daterange in self.cleaned_data['many_dateranges']])
+
+        else:  # infinite repetitions
+            pass  # gros bordel
 
 
 class TagForm(forms.ModelForm):
