a15f2c9804b2131348fdb1767dafe455acc18daa
Guilouf <Guilouf@users.noreply.github.com>
2018-12-03 19:06:34 +0100

    Modification of scheduled tasks now functional, appends new tasks

--- hostproject/agendjang/forms.py
+++ hostproject/agendjang/forms.py
@@ -13,22 +13,32 @@ class TaskForm(forms.ModelForm):
 class ScheduledTaskForm(forms.ModelForm):
     CHRON_CHOICE = [('days', 'Day'), ('weeks', 'Week'), ('months', 'Month'), ('years', 'Year')]
     chronicity = forms.ChoiceField(choices=CHRON_CHOICE)
-    repeats = forms.IntegerField(min_value=0)  # how many times its repeated (0 infinite)
+    repeats = forms.IntegerField(min_value=0, initial=0)  # how many times its repeated (0 infinite)
 
     class Meta:
         model = ScheduledTask
         fields = '__all__'
+        exclude = ('many_tasks',)
 
     def clean(self):
         """
         Instantiate the tasks based on the scheduled task
         """
         if self.cleaned_data['repeats'] != 0:  # finite number of repetitions
-            for i in range(1, self.cleaned_data['repeats']+1):
+
+            try:
+                task_name = self.instance.many_tasks.last().name
+                index = int(task_name.split('_')[-1])  # get the task "number"
+            except AttributeError:  # there's no similar existing task
+                index = 1
+
+            for i in range(index, self.cleaned_data['repeats']+index):
                 task = Task.objects.create(name=f"{self.cleaned_data['name']}_{i+1}",  # first 1 = linked with schedtask
                                            description=self.cleaned_data['description'],
                                            points=self.cleaned_data['points'])
                 task.many_tags.set(list(self.cleaned_data['many_tags']))
+
+                self.instance.many_tasks.add(task)
 
                 if self.cleaned_data['many_dateranges'].exists():  # checks if the inherited task has a daterange
                     chron = {self.cleaned_data['chronicity']: i}  # kwargs for relativedelta
