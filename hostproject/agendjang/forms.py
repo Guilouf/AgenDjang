9e264b50a6e31bb1b0953c1ae9ae894e7e730b0e
Guilouf <Guilouf@users.noreply.github.com>
2018-12-03 23:49:30 +0100

    Db rollback by transaction, handled the case where Schedtask was not yet created, worked on form fields rather than instance ones

--- hostproject/agendjang/forms.py
+++ hostproject/agendjang/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from agendjang.models import Task, ScheduledTask, Tag
+from django.db import transaction
 
 from dateutil.relativedelta import relativedelta
 
@@ -19,35 +20,37 @@ class ScheduledTaskForm(forms.ModelForm):
     class Meta:
         model = ScheduledTask
         fields = '__all__'
-        exclude = ('many_tasks',)
 
     def clean(self):
         """
         Instantiate the tasks based on the scheduled task
         """
-        if self.cleaned_data['repeats'] != 0:  # finite number of repetitions
+        with transaction.atomic():
+            if self.cleaned_data['repeats'] != 0:  # finite number of repetitions
+                self.cleaned_data['many_tasks'] = list(self.cleaned_data['many_tasks'])
 
-            try:
-                task_name = self.instance.many_tasks.last().name
-                index = int(task_name.split('_')[-1])  # get the task "number"
-            except AttributeError:  # there's no similar existing task
-                index = 1
+                try:
+                    task_name = self.instance.many_tasks.last().name
+                    # by instance ok because by definition always in update, ValueError in case of create
+                    index = int(task_name.split('_')[-1])  # get the task "number"
+                except (AttributeError, ValueError):  # there's no similar existing task
+                    index = 1
 
-            for i in range(index, self.cleaned_data['repeats']+index):
-                task = Task.objects.create(name=f"{self.cleaned_data['name']}_{i+1}",  # first 1 = linked with schedtask
-                                           description=self.cleaned_data['description'],
-                                           points=self.cleaned_data['points'])
-                task.many_tags.set(list(self.cleaned_data['many_tags']))
+                for i in range(index, self.cleaned_data['repeats']+index):
+                    task = Task.objects.create(name=f"{self.cleaned_data['name']}_{i+1}",  # first 1 = linked with schedtask
+                                               description=self.cleaned_data['description'],
+                                               points=self.cleaned_data['points'])
+                    task.many_tags.set(list(self.cleaned_data['many_tags']))
 
-                self.instance.many_tasks.add(task)
+                    self.cleaned_data['many_tasks'].append(task)
 
-                if self.cleaned_data['many_dateranges'].exists():  # checks if the inherited task has a daterange
-                    chron = {self.cleaned_data['chronicity']: i}  # kwargs for relativedelta
-                    # then copy and add a timedelta to these dateranges for the new task (cf Dateranges __add__)
-                    task.many_dateranges.set([(daterange + relativedelta(**chron)).save()  # no full_clean, should be ok
-                                              for daterange in self.cleaned_data['many_dateranges']])
-                else:
-                    raise ValidationError({'many_dateranges': 'Scheduled Tasks needs at least one DateRange'})
+                    if self.cleaned_data['many_dateranges'].exists():  # checks if the inherited task has a daterange
+                        chron = {self.cleaned_data['chronicity']: i}  # kwargs for relativedelta
+                        # then copy and add a timedelta to these dateranges for the new task (cf Dateranges __add__)
+                        task.many_dateranges.set([(daterange + relativedelta(**chron)).save()  # no full_clean, should be ok
+                                                  for daterange in self.cleaned_data['many_dateranges']])
+                    else:
+                        raise ValidationError({'many_dateranges': 'Scheduled Tasks needs at least one DateRange'})
 
         else:  # infinite repetitions
             pass  # gros bordel
