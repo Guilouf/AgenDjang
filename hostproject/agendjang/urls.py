482d75857a83689f556bc916a4c88df2e7c26e6b
Guilouf <Guilouf@users.noreply.github.com>
2021-11-09 09:24:13 +0100

    New style urls

--- hostproject/agendjang/urls.py
+++ hostproject/agendjang/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from agendjang import views
 from rest_framework import routers
 
@@ -11,28 +11,27 @@ router.register(r'dateranges', views.DateRangeViewSet, basename='dateranges')
 
 urlpatterns = [
     # api root path
-    url(r'^api/', include((router.urls, 'api')), ),  # optional url namespace
+    path('api/', include((router.urls, 'api')), ),  # optional url namespace
 
     # Other urls
-    url(r'^help$', views.help_view, name='help'),
+    path('help', views.help_view, name='help'),
 
     # calendars
-    url(r'^$', views.CalendarView.as_view(), name='view_calendar'),
-    url(r'^js_calendar$', views.JavascriptCalendarView.as_view(), name='view_js_calendar'),
+    path('', views.CalendarView.as_view(), name='view_calendar'),
+    path('js_calendar', views.JavascriptCalendarView.as_view(), name='view_js_calendar'),
 
     # Tasks
-    url(r'^create_task$', views.TaskCreate.as_view(), name='create_task'),
-    url(r'^update_task/(?P<pk>\d+)$', views.TaskUpdate.as_view(), name='update_task'),
-    url(r'^tasks$', views.TaskList.as_view(), name='list_task'),
-    url(r'^tasks/(?P<pk>\d+)$', views.TaskDetail.as_view(), name='detail_task'),
+    path('create_task', views.TaskCreate.as_view(), name='create_task'),
+    path('update_task/<int:pk>', views.TaskUpdate.as_view(), name='update_task'),
+    path('tasks', views.TaskList.as_view(), name='list_task'),
+    path('tasks/<int:pk>', views.TaskDetail.as_view(), name='detail_task'),
 
     # ScheduledTasks
-    url(r'^create_schedtask$', views.ScheduledTaskCreate.as_view(), name='create_schedtask'),
-    url(r'^update_schedtask/(?P<pk>\d+)$', views.ScheduledTaskUpdate.as_view(), name='update_schedtask'),
+    path('create_schedtask', views.ScheduledTaskCreate.as_view(), name='create_schedtask'),
+    path('update_schedtask/<int:pk>', views.ScheduledTaskUpdate.as_view(), name='update_schedtask'),
 
     # Tags
-    url(r'^create_tag$', views.TagCreate.as_view(), name='create_tag'),
-    url(r'^update_tag/(?P<pk>\d+)$', views.TagUpdate.as_view(), name='update_tag'),
-
+    path('create_tag', views.TagCreate.as_view(), name='create_tag'),
+    path('update_tag/<int:pk>', views.TagUpdate.as_view(), name='update_tag'),
 
 ]
