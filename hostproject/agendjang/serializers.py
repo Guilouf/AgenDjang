6dabc97813e135c9180cc1ef5e6dea3e46e356bf
Guilouf <Guilouf@users.noreply.github.com>
2021-11-16 15:42:26 +0100

    Removed stupid show off with metaclass

--- hostproject/agendjang/serializers.py
+++ hostproject/agendjang/serializers.py
@@ -1,24 +1,14 @@
-import sys
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from agendjang.models import Task, DateRange
 
 
-module = sys.modules['agendjang.models']
-
-
-def meta_serializer(name, bases, dct):
-    bases += serializers.ModelSerializer,  # append the base class tuple
-
+class TaskSerializer(ModelSerializer):
     class Meta:
-        model = getattr(module, name)
+        model = Task
         fields = '__all__'
 
-    dct = dict(dct, **{Meta.__name__: Meta})  # merge of the dicts
 
-    return type(f"{name}Serializer", bases, dct)
-
-
-SERIALIZERS = ['Task', 'DateRange']  # edit to create new serializers
-
-# set the new var
-for serial in SERIALIZERS:  # pas reussi a faire plus classe en intention, c'est plus moche en fait
-    globals()[f'{serial}Serializer'] = meta_serializer(serial, (), {})
+class DateRangeSerializer(ModelSerializer):
+    class Meta:
+        model = DateRange
+        fields = '__all__'
